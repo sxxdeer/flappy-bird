750687747db153b11cfbe340881c79ec000bec9d
sxxdeer <104995696+sxxdeer@users.noreply.github.com>
2022-05-05 17:04:11 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1651758726,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/trouble-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/background-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
